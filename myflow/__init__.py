b8a6bf79eb6481564d4d432e8eb352d122e06a3a
wangchen <447648965@qq.com>
2019-12-08 15:56:12 +0800

    1.项目文件结构调整 2.实现了simoid，minus等操作 3.操作符重载

---
+++ myflow/__init__.py
@@ -0,0 +1,4 @@
+from myflow.ops import add, mul, matmul, transpose, reduce_sum, broadcast, oneslike, zeroslike, placeholder, variable, \
+    constant, minus, sigmoid
+from myflow.session import Session
+from myflow.optimizer import Optimizer
