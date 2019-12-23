ad1069a7e9adc21b859cc5e0a4ed2cfad2f638ac
wangchen1 <wangchen1@wisetv.com.cn>
2019-12-23 09:16:27 +0800

    add add_bias

--- myflow/nn.py
+++ myflow/nn.py
@@ -1,7 +1,8 @@
-from myflow.ops import add,broadcast
-import tensorflow as tf
+from myflow.ops import add, broadcast, reshape
 
-tf.nn.bias_add()
-def add_bias(x,bias):
-    return  x + broadcast(bias, shape=[10, 1], axis=0, tensor_name="b2_broadcast")
 
+def add_bias(x, bias, tensor_name):
+    assert len(bias.shape) == 1 and bias.shape[0] == x.shape[1]
+    bias = reshape(bias, shape=[1, bias.shape[0]])
+    return add(x, broadcast(bias, shape=x.shape, axis=0, tensor_name="b2_broadcast"),
+               tensor_name=tensor_name)
